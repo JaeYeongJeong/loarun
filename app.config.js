1ff74e228690a1ab60392ef635c14273b182dd0b
JaeYeong Jeong <94372210+JaeYeongJeong@users.noreply.github.com>
2025-07-23 17:13:21 +0900

    version update 1.0.8

--- app.config.js
+++ app.config.js
@@ -12,7 +12,7 @@ export default {
     },
     userInterfaceStyle: "automatic",
     icon: "./assets/images/loarunIcon-1024-big-removebg.png",
-    version: "1.0.7",
+    version: "1.0.8",
     newArchEnabled: true,
     splash: {
       image: "./assets/images/loarunIcon-1024-big-removebg.png",
